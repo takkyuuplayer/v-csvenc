f5bc1507398f79d9e5b95ac1d39442aed19e8826
takkyuuplayer <takkyuuplayer@gmail.com>
2021-11-07 20:37:21 +0900

    Port of writer

---
+++ writer.v
@@ -0,0 +1,90 @@
+module csvenc
+
+import takkyuuplayer.bufwriter
+import io
+
+// A Writer writes records using CSV encoding.
+pub struct Writer {
+	delimiter    rune
+	use_crlf     bool
+	always_quote bool
+mut:
+	bw bufwriter.Writer
+}
+
+// Config are options that can be given to a writer
+pub struct Config {
+	writer       io.Writer
+	delimiter    rune = `,`
+	use_crlf     bool
+	always_quote bool
+}
+
+pub fn new_writer(c Config) ?&Writer {
+	if !valid_delim(c.delimiter) {
+		return error('invalid delimiter: $c.delimiter')
+	}
+
+	return &Writer{
+		bw: bufwriter.new(writer: c.writer)
+		delimiter: c.delimiter
+		use_crlf: c.use_crlf
+		always_quote: c.always_quote
+	}
+}
+
+pub fn (mut w Writer) write(fields []string) ? {
+	le := if w.use_crlf { '\r\n' } else { '\n' }
+	for n, field_ in fields {
+		mut field := field_
+		if n > 0 {
+			w.bw.write(w.delimiter.str().bytes()) ?
+		}
+		if !w.field_needs_quotes(field) {
+			_ = w.bw.write(field.bytes()) ?
+			continue
+		}
+		w.bw.write('"'.bytes()) ?
+		for field.len > 0 {
+			mut i := field.index_any('"\r\n')
+			if i < 0 {
+				i = field.len
+			}
+			w.bw.write(field[..i].bytes()) ?
+			field = field[i..]
+			if field.len > 0 {
+				z := field[0]
+				match z {
+					`"` { w.bw.write('""'.bytes()) ? }
+					`\r`{ if !w.use_crlf { w.bw.write('\r'.bytes()) ? } }
+					`\n`{ w.bw.write(le.bytes()) ? }
+					else {}
+				}
+				field = field[1..]
+			}
+		}
+		w.bw.write('"'.bytes()) ?
+	}
+	w.bw.write(le.bytes()) ?
+}
+
+pub fn (mut w Writer) flush() ? {
+	w.bw.flush() ?
+}
+
+fn (w &Writer) field_needs_quotes(field string) bool {
+	if field == '' {
+		return false
+	}
+	if field == '\\.' {
+		return true
+	}
+	if field.contains(w.delimiter.str()) || (field.index_any('"\r\n') != -1) {
+		return true
+	}
+	return false
+}
+
+fn valid_delim(b rune) bool {
+	return b != 0 && b != `"` && b != `\r` && b != `\n` && b < 255
+}
---
+++ writer_test.v
@@ -0,0 +1,153 @@
+module csvenc
+
+import takkyuuplayer.bytebuf
+
+fn test_new_writer() ? {
+	w := bytebuf.Buffer{}
+	{
+		writer := new_writer(writer: w) ?
+
+		assert writer.delimiter == `,`
+		assert writer.use_crlf == false
+	}
+	{
+		// with config
+		writer := new_writer(writer: w, delimiter: `\t`, use_crlf: true) ?
+
+		assert writer.delimiter == `\t`
+		assert writer.use_crlf == true
+	}
+	{
+		// with invalid delimiter
+		if writer := new_writer(writer: w, delimiter: `あ`) {
+			assert false
+		} else {
+			assert err.str() == 'invalid delimiter: あ'
+		}
+	}
+}
+
+struct WriteTestCase {
+	input     []string
+	output    string
+	use_crlf  bool
+	delimiter rune = `,`
+}
+
+fn test_write() ? {
+	cases := [
+		WriteTestCase{
+			input: ['abc']
+			output: 'abc\n'
+		},
+		WriteTestCase{
+			input: ['abc']
+			output: 'abc\r\n'
+			use_crlf: true
+		},
+		WriteTestCase{
+			input: ['"abc"']
+			output: '"""abc"""\n'
+		},
+		WriteTestCase{
+			input: ['a"b']
+			output: '"a""b"\n'
+		},
+		WriteTestCase{
+			input: [' abc']
+			output: ' abc\n'
+		},
+		WriteTestCase{
+			input: ['abc,def']
+			output: '"abc,def"\n'
+		},
+		WriteTestCase{
+			input: ['abc\ndef']
+			output: '"abc\ndef"\n'
+		},
+		WriteTestCase{
+			input: ['abc\ndef']
+			output: '"abc\r\ndef"\r\n'
+			use_crlf: true
+		},
+		WriteTestCase{
+			input: ['abc\rdef']
+			output: '"abc\rdef"\n'
+		},
+		WriteTestCase{
+			input: ['abc\rdef']
+			output: '"abcdef"\r\n'
+			use_crlf: true
+		},
+		WriteTestCase{
+			input: ['']
+			output: '\n'
+		},
+		WriteTestCase{
+			input: ['', '']
+			output: ',\n'
+		},
+		WriteTestCase{
+			input: ['', '', '']
+			output: ',,\n'
+		},
+		WriteTestCase{
+			input: ['', '', 'a']
+			output: ',,a\n'
+		},
+		WriteTestCase{
+			input: ['', 'a', '']
+			output: ',a,\n'
+		},
+		WriteTestCase{
+			input: ['', 'a', 'a']
+			output: ',a,a\n'
+		},
+		WriteTestCase{
+			input: ['a', '', '']
+			output: 'a,,\n'
+		},
+		WriteTestCase{
+			input: ['a', '', 'a']
+			output: 'a,,a\n'
+		},
+		WriteTestCase{
+			input: ['a', 'a', '']
+			output: 'a,a,\n'
+		},
+		WriteTestCase{
+			input: ['a', 'a', 'a']
+			output: 'a,a,a\n'
+		},
+		WriteTestCase{
+			input: ['\\.']
+			output: '"\\."\n'
+		},
+		WriteTestCase{
+			input: ['x09\x41\xb4\x1c', 'aktau']
+			output: 'x09\x41\xb4\x1c,aktau\n'
+		},
+		WriteTestCase{
+			input: [',x09\x41\xb4\x1c', 'aktau']
+			output: '",x09\x41\xb4\x1c",aktau\n'
+		},
+		WriteTestCase{
+			input: ['a', 'a', '']
+			output: 'a|a|\n'
+			delimiter: `|`
+		},
+		WriteTestCase{
+			input: [',', ',', '']
+			output: ',|,|\n'
+			delimiter: `|`
+		},
+	]
+
+	for _, tt in cases {
+		w := bytebuf.Buffer{}
+		mut writer := new_writer(writer: w, use_crlf: tt.use_crlf, delimiter: tt.delimiter) ?
+		writer.write(tt.input) ?
+		writer.flush() ?
+		assert w.bytes().bytestr() == tt.output
+	}
+}
