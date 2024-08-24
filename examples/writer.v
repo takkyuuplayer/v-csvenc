4d1dfe6b86e2ce1368296d4e86a9bee7a035929f
takkyuuplayer <takkyuuplayer@gmail.com>
2024-08-24 18:57:46 +0900

    bump version

--- examples/writer.v
+++ examples/writer.v
@@ -1,9 +1,9 @@
 import os
-import examples.csvenc
+import csvenc
 
 fn main() {
 	mut output := os.stdout()
-	mut buf := csvenc.new_writer(writer: output)?
-	buf.write(['a', 'b', 'c'])?
-	buf.flush()? // Output: a,b,c
+	mut buf := csvenc.new_writer(writer: output)!
+	buf.write(['a', 'b', 'c'])!
+	buf.flush()! // Output: a,b,c
 }
